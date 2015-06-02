c460a66b24af6e356865b8a0d14c95e0fb295d6c
keithbarrow <keith.barrow@tombola.com>
2015-06-02 15:23:41 +0100

    Added gruntfile with copy tasks

---
+++ gruntfile.js
@@ -0,0 +1,33 @@
+(function () {
+    'use strict';
+    module.exports = function(grunt){
+        grunt.initConfig({
+            pkg: grunt.file.readJSON('package.json'),
+            copy: {
+
+                mainappimages: {
+                    cwd:'',
+                    src: ['main-app/app/images/**/*.*'],
+                    dest: '.build/',
+                    expand: true
+                },
+
+                mainappsounds: {
+                    cwd:'',
+                    src: ['main-app/app/sounds/*.*'],
+                    dest: '.build/',
+                    expand: true
+                },
+
+                bower: {
+                    cwd:'bower_components',
+                    src: ['angular/*.*'],
+                    dest: '.build/main-app//app/thirdparty/',
+                    expand:true
+                }
+            }
+        });
+        grunt.loadNpmTasks('grunt-contrib-copy');
+        grunt.registerTask('default', ['copy']);
+    };
+})();
